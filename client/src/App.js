7a5f4a64968b1328a7995bf054198d04089e84fe
antonio.correa1 <antonio.correa1@upr.edu>
2019-11-15 10:30:55 -0400

    change in folders

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import About from "./components/pages/About";
 import Navbar from "./components/layout/Navbar";
 
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
